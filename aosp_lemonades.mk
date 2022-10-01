d4f8d3cb35ec25642b24df8e2152aa4376a1f6c3
schqiushui <orochi9999@gmail.com>
2022-10-01 15:38:13 +0800

    lemonades: set 'CUSTOM_BUILD_TYPE' to 'OFFICIAL' for personal OTA
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- aosp_lemonades.mk
+++ aosp_lemonades.mk
@@ -27,6 +27,9 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
+#Custom build type
+CUSTOM_BUILD_TYPE := OFFICIAL
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lemonades
 PRODUCT_DEVICE := lemonades
